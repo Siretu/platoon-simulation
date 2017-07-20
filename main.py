0d8bdce0bbbebf31a45ec9a0de4ab7bb9c1f192a
Erik Ihren <eihren@kth.se>
2017-07-20 23:44:05 +0200

    Added main file

---
+++ main.py
@@ -0,0 +1,13 @@
+from platooning.platooning_methods import GreedyPlatooning
+from route_calculation import get_path_data_sets
+from run_simulation import dynamic_simulation
+
+
+def main():
+    path_data = get_path_data_sets('./testing/testroutes/test100-1-new/')
+    result = dynamic_simulation(GreedyPlatooning(), path_data_sets=path_data, folder='./testing/testroutes/test100-1-new/')
+    print sum([x.current_fuel_consumption() for x in result]) / sum([x.default_plan.fuel for x in result])
+
+
+if __name__ == "__main__":
+    main()
